5f8d6efb811872a476d164b05395c2ad218a6997
M <MPbugaga@yandex.ru>
2021-06-13 21:50:53 +0300

    fix start script

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -4,4 +4,5 @@ start /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5002"
 start /d ..\nginx\ nginx.exe
 start /d ..\nats\ nats-server.exe
 
-start /d ..\RankCalculator\ dotnet run
+start "Rank calculator 1" /d ..\RankCalculator\ dotnet run 
+start "Rank calculator 2" /d ..\RankCalculator\ dotnet run 
