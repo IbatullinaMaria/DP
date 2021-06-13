8d1fc010b4708ba43aaa46edae2b0c016bfc2189
M <MPbugaga@yandex.ru>
2021-06-13 21:47:37 +0300

    bug fix

--- EventsLogger/Program.cs
+++ EventsLogger/Program.cs
@@ -1,18 +1,3 @@
-/*using System;
-using Valuator;
-namespace EventsLogger
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            NatsSubscriber natsSubscriber = new NatsSubscriber();
-            EventsLogger eventsLogger = new EventsLogger(natsSubscriber);
-            eventsLogger.Start();      
-        }
-    }
-}*/
-
 using Library;
 using NATS.Client;
 using System;
